c00adc6c4d6b6b2b5d8f1b120ba43f1913a9df1a
Eddie Atkinson <45678264+eddie-atkinson@users.noreply.github.com>
2025-03-29 20:46:36 +0800

    Add back benchmark file

---
+++ main.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from pandas_should_go_extinct.Benchmark import Benchmark
+
+
+DUCKDB_PATH = Path("./duckdb_1brc.py")
+DUCKDB_OUTPUT_PATH = Path("./data/duckdb_timeseries.csv")
+
+POLARS_PATH = Path("./polars_1brc.py")
+POLARS_OUTPUT_PATH = Path("./data/polars_timeseries.csv")
+
+PANDAS_PATH = Path("./pandas_1brc.py")
+PANDAS_OUTPUT_PATH = Path("./data/pandas_timeseries.csv")
+
+
+PROFILE_FIXTURES = [
+    (POLARS_PATH, POLARS_OUTPUT_PATH),
+    # (DUCKDB_PATH, DUCKDB_OUTPUT_PATH),
+    # (PANDAS_PATH, PANDAS_OUTPUT_PATH),
+]
+
+
+def main():
+    for test_script, output_path in PROFILE_FIXTURES:
+        print(f"Running benchmark script {test_script} with output in {output_path}")
+        benchmark = Benchmark(
+            script_path=test_script, output_file_path=output_path, n_warmup=2, n_iter=5, peek_ms=100
+        )
+        benchmark.run()
+
+
+if __name__ == "__main__":
+    main()
--- src/pandas_should_go_extinct/hybrid_duck_brc.py
+++ src/pandas_should_go_extinct/hybrid_duck_brc.py
@@ -7,12 +7,13 @@ def do_1brc_hybrid_duck(file_path: str, output_data=True) -> str:
         file_path, sep=";", names=["station", "measurement"], nrows=500_000
     )
 
-    output_df = duckdb.sql("select station, min(measurement) min, cast(avg(measurement) as decimal(8, 1)) avg, max(measurement) max from df group by station order by station").df()
-
+    output_df = duckdb.sql(
+        "select station, min(measurement) min, cast(avg(measurement) as decimal(8, 1)) avg, max(measurement) max from df group by station order by station"
+    ).df()
 
     if output_data:
         result = []
-        for _,station, min_val, mean_val, max_val in output_df.to_records():
+        for _, station, min_val, mean_val, max_val in output_df.to_records():
             result.append(f"{station}={min_val}/{mean_val}/{max_val}")
         return "{" + ", ".join(result) + "}"
     return ""
